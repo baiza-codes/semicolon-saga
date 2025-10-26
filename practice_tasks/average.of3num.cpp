200b4360275f7c532c285893a3ac6e453839883d
baiza-codes <baizaislam1@gmail.com>
2025-10-26 12:11:53 +0500

    Add program to calculate average of three numbers

---
+++ practice_tasks/average.of3num.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b, c;
+    cout << "Enter 3 numbers: ";
+    cin>> a>>b>>c;
+    cout <<"Average is: "<< (a+b+c)/3<< endl;
+
+
+
+    return 0;
+}
