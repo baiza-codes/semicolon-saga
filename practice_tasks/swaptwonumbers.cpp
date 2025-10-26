0c5c3ec34de19e6dca42ec2b1d28b04e2ea39f16
baiza-codes <baizaislam1@gmail.com>
2025-10-26 13:44:17 +0500

    add swap function for better readability

--- practice_tasks/swaptwonumbers.cpp
+++ practice_tasks/swaptwonumbers.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+// #include <utility> used for the function swap(a, b) coz values of a and b could overflow (go beyonf int range)
 using namespace std;
 
 int main(){
@@ -7,6 +8,7 @@ int main(){
     cin>>a>>b;
     cout << "Before swap: " << "a = " << a << " b = " << b <<endl;
     // swaping a and b 
+    // swap(a, b) better practice
     a = a + b ;
     b = a - b ;
     a = a - b ;
